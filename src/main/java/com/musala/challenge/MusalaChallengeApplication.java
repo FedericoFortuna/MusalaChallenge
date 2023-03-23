47ea5728bf27aaeb7c0c5503188b60ed9af74ec3
FedericoFortuna <fedekpg17@gmail.com>
2023-03-22 23:51:24 -0300

    FirstCommit

---
+++ src/main/java/com/musala/challenge/MusalaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.musala.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusalaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusalaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musala/challenge/MusalaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusalaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
