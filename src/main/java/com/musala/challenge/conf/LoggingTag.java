28c1f68c9d8a85ab7dae4399dbea63a1ef70a691
FedericoFortuna <fedekpg17@gmail.com>
2023-03-23 14:25:53 -0300

    add tags

--- src/main/java/com/musala/challenge/conf/LoggingTag.java
+++ src/main/java/com/musala/challenge/conf/LoggingTag.java
@@ -1,2 +1,5 @@
-package com.musala.challenge.conf;public class LoggingTag {
+package com.musala.challenge.conf;
+
+public class LoggingTag {
+    public static final String SERVICE_EXECUTION = "[SERVICE_EXECUTION]";
 }
