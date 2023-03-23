d85450e4c9a3fa4492f6b892159d4fdbec8380f1
FedericoFortuna <fedekpg17@gmail.com>
2023-03-23 14:28:40 -0300

    add repo for drone

--- src/main/java/com/musala/challenge/repositories/DroneRepository.java
+++ src/main/java/com/musala/challenge/repositories/DroneRepository.java
@@ -2,7 +2,9 @@ package com.musala.challenge.repositories;
 
 import com.musala.challenge.entities.Drone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DroneRepository extends JpaRepository<Drone, String> {
 
 }
