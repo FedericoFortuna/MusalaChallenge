6ac0e6731b86099d355f0386df82d3b65cc67943
FedericoFortuna <fedekpg17@gmail.com>
2023-03-24 16:12:51 -0300

    add regex for medications validation

--- src/main/java/com/musala/challenge/utils/RegexMedications.java
+++ src/main/java/com/musala/challenge/utils/RegexMedications.java
@@ -1,2 +1,8 @@
-package com.musala.challenge.utils;public class RegexMedications {
+package com.musala.challenge.utils;
+
+import jakarta.validation.constraints.Pattern;
+
+public class RegexMedications {
+    public static String REGEX_ID = "^[a-zA-Z0-9_-]+$";
+    public static String REGEX_CODE = "^[A-Z0-9_]+$";
 }
