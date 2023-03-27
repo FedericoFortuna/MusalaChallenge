a80b4899194846162f436b27f3f8368eb860f48b
FedericoFortuna <fedekpg17@gmail.com>
2023-03-26 22:21:01 -0300

    add logging into a log file

--- src/main/java/com/musala/challenge/conf/LoggingTag.java
+++ src/main/java/com/musala/challenge/conf/LoggingTag.java
@@ -2,4 +2,7 @@ package com.musala.challenge.conf;
 
 public class LoggingTag {
     public static final String SERVICE_EXECUTION = "[SERVICE_EXECUTION]";
+    public static final String AUDIT_BATTERY = "[AUDIT_BATTERY_LEVEL]";
+
+    public static final String CONFIGURATION = "[CONFIGURATION]";
 }
