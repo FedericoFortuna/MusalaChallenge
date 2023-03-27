7ca2b93e3f8b262a5d6133a36ccf0fde3df7d44a
FedericoFortuna <fedekpg17@gmail.com>
2023-03-26 22:22:34 -0300

    add task to log into a file once a day

--- src/main/java/com/musala/challenge/tasks/AuditBattery.java
+++ src/main/java/com/musala/challenge/tasks/AuditBattery.java
@@ -1,2 +1,41 @@
-package com.musala.challenge.tasks;public class AuditBattery {
+package com.musala.challenge.tasks;
+
+import com.musala.challenge.conf.LoggingTag;
+import com.musala.challenge.dtos.Drone;
+import com.musala.challenge.mappers.DroneMapper;
+import com.musala.challenge.repositories.DroneRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class AuditBattery {
+
+
+    private final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(AuditBattery.class);
+    @Autowired
+    private DroneRepository droneRepository;
+
+    @Autowired
+    private DroneMapper droneMapper;
+
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void checkDronesBatteryLevelTask() {
+        this.getDrones().forEach(drone -> logger.info(LoggingTag.AUDIT_BATTERY + " : Battery level for drone " +  drone.getNumber() + " is " + drone.getBatteryCapacity() + "%"));
+    }
+
+
+    private List<Drone> getDrones() {
+        List<com.musala.challenge.entities.Drone> droneList = droneRepository.findAll();
+        List<Drone> drones = new ArrayList<>();
+        droneList.forEach(drone -> drones.add(droneMapper.toDto(drone)));
+        return drones;
+    }
+
 }
