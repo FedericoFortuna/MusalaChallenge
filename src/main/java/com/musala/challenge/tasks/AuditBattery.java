8a71c7722c4923568bb4704c80d7912e5a8880c0
FedericoFortuna <fedekpg17@gmail.com>
2023-03-30 22:26:01 -0300

    add log

--- src/main/java/com/musala/challenge/tasks/AuditBattery.java
+++ src/main/java/com/musala/challenge/tasks/AuditBattery.java
@@ -5,11 +5,14 @@ import com.musala.challenge.dtos.Drone;
 import com.musala.challenge.mappers.DroneMapper;
 import com.musala.challenge.repositories.DroneRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,14 +22,17 @@ public class AuditBattery {
 
 
     private final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(AuditBattery.class);
+    private final Logger log = LoggerFactory.getLogger(AuditBattery.class);
+    private final String LOG_AUDIT_BATTERY_LEVEL = "{} {} - {}: AUDIT BATTERY LEVEL INTO A LOG";
     @Autowired
     private DroneRepository droneRepository;
 
     @Autowired
     private DroneMapper droneMapper;
 
-    @Scheduled(cron = "0 */5 * ? * *")
+    @Scheduled(cron = "0 */1 * ? * *")
     public void checkDronesBatteryLevelTask() {
+        log.info(LOG_AUDIT_BATTERY_LEVEL, LocalDateTime.now(), LoggingTag.AUDIT_BATTERY, AuditBattery.class.getSimpleName());
         this.getDrones().forEach(drone -> logger.info(LoggingTag.AUDIT_BATTERY + " : Battery level for drone " +  drone.getNumber() + " is " + drone.getBatteryCapacity() + "%"));
     }
 
