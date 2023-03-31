c96fc28dc8a7a49cac37025f241d59f912d69142
FedericoFortuna <fedekpg17@gmail.com>
2023-03-30 21:31:10 -0300

    refactor return types methods

--- src/main/java/com/musala/challenge/controllers/DroneController.java
+++ src/main/java/com/musala/challenge/controllers/DroneController.java
@@ -22,34 +22,34 @@ public class DroneController {
     private IMedicationService iMedicationService;
 
     @PostMapping(value = "/create")
-    public ResponseEntity<String> createDrone(@RequestBody Drone drone){
-        return new ResponseEntity<>(iDroneService.createDrone(drone).toString(), HttpStatus.OK);
+    public ResponseEntity<Drone> createDrone(@RequestBody Drone drone){
+        return new ResponseEntity<>(iDroneService.createDrone(drone), HttpStatus.OK);
     }
 
     @PutMapping(value = "/{serialNumber}/load")
-    public ResponseEntity<String> loadDroneWithMedications(@PathVariable String serialNumber,
+    public ResponseEntity<Drone> loadDroneWithMedications(@PathVariable String serialNumber,
                                                            @RequestBody List<Medication> medications){
 
-        return new ResponseEntity<>(iDroneService.loadDrone(serialNumber, medications).toString(), HttpStatus.OK);
+        return new ResponseEntity<>(iDroneService.loadDrone(serialNumber, medications), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{serialNumber}/check")
-    public ResponseEntity<String> checkMedicationsLoaded(@PathVariable String serialNumber){
-        return new ResponseEntity<>(iMedicationService.checkLoadedMedication(serialNumber).toString(), HttpStatus.OK);
+    public ResponseEntity<List<Medication>> checkMedicationsLoaded(@PathVariable String serialNumber){
+        return new ResponseEntity<>(iMedicationService.checkLoadedMedication(serialNumber), HttpStatus.OK);
     }
 
     @GetMapping(value = "/available")
-    public ResponseEntity<String> getAvailablesDronesForLoading(){
-        return new ResponseEntity<>(iDroneService.getDronesForLoading().toString(), HttpStatus.OK);
+    public ResponseEntity<List<Drone>> getAvailablesDronesForLoading(){
+        return new ResponseEntity<>(iDroneService.getDronesForLoading(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{serialNumber}/battery")
-    public ResponseEntity<String> getDroneBattery(@PathVariable String serialNumber){
-        return new ResponseEntity<>(iDroneService.getDroneBattery(serialNumber).toString(), HttpStatus.OK);
+    public ResponseEntity<Integer> getDroneBattery(@PathVariable String serialNumber){
+        return new ResponseEntity<>(iDroneService.getDroneBattery(serialNumber), HttpStatus.OK);
     }
 
     @PatchMapping(value = "/{serialNumber}/loading")
-    public ResponseEntity changeStatusToLoading(@PathVariable String serialNumber){
+    public ResponseEntity<HttpStatus> changeStatusToLoading(@PathVariable String serialNumber){
         iDroneService.changeStatusToLoading(serialNumber);
         return new ResponseEntity<>(HttpStatus.OK);
     }
